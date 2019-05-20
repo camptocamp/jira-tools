acf678efc092bd085c313c66ef90c9c5d05a55f6
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-05-20 16:37:10 +0200

    fixup! fixup! Add auth and list jira project [PR started on odoo_utils]

--- github_common.py
+++ github_common.py
@@ -10,15 +10,7 @@ def repositories_by_topic(github, topic):
         :return: repository list
     """
     repos = []
-    topic_text =  _format_topic(topic)
+    topic_text =  ' '.join(topic[0].split(','))
     for repo in github.search_repositories(f"topic:{topic_text}"):
         repos.append(repo.repository.name)
     return repos
-
-def _format_topic(topic_tuple):
-    """ Format to render topic in a proper domain string for GitHub
-
-        cause 'tuple' object has no attribute 'replace'
-    """
-    topic_text =  ' '.join(topic_tuple)
-    return ' '.join(topic_text.split(','))
